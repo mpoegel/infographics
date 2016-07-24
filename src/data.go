c6aa72656aa8b49d45554b9fbab158ebc4844273
Matt Poegel <mpoegel@comcast.net>
2016-07-24 11:06:21 -0700

    added script to fetch data; script to create 2 new plots

---
+++ src/data.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+const wpPoliceShootings = "https://raw.githubusercontent.com/washingtonpost/data-police-shootings/master/fatal-police-shootings-data.csv"
+
+// Pull a data source down from a uri and save it in ./data
+func fetchData(uri string, outName string) {
+	response, err := http.Get(uri)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		defer response.Body.Close()
+		// create the data directory if it does not already exist
+		if err := os.Mkdir("data", os.ModeDir); err != nil {
+			log.Printf("data directory already exists")
+		}
+		// create the output file
+		fo, err := os.Create("data/" + outName)
+		if err != nil {
+			panic(err)
+		}
+		defer func() {
+			if err := fo.Close(); err != nil {
+				panic(err)
+			}
+		}()
+		// make a write buffer
+		w := bufio.NewWriter(fo)
+		// make a read buffer
+		buf := make([]byte, 1024)
+		for {
+			n, err := response.Body.Read(buf)
+			if err != nil && err != io.EOF {
+				panic(err)
+			}
+			if n == 0 {
+				break
+			}
+			// write to the out file buffer
+			if _, err := w.Write(buf[:n]); err != nil {
+				panic(err)
+			}
+		}
+		if err := w.Flush(); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func main() {
+	fetchData(wpPoliceShootings, "wp-police-shootings.csv")
+}
---
+++ src/police-shootings.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gonum/plot"
+	"github.com/gonum/plot/plotter"
+	"github.com/gonum/plot/plotutil"
+	"github.com/gonum/plot/vg"
+	"github.com/mpoegel/gota/data-frame"
+)
+
+const dateFormat = "2016-Jul-24"
+
+func main() {
+	d := df.DataFrame{}
+
+	// open the data file
+	absPath, _ := filepath.Abs("data/wp-police-shootings.csv")
+	csvfile, err := os.Open(absPath)
+	if err != nil {
+		panic(err)
+	}
+	r := csv.NewReader(csvfile)
+	records, err := r.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	// load the data as columns
+	types := df.T{
+		"id":                      "int",
+		"name":                    "string",
+		"date":                    "string",
+		"manner_of_death":         "string",
+		"armed":                   "string",
+		"age":                     "int",
+		"gender":                  "string",
+		"race":                    "string",
+		"city":                    "string",
+		"state":                   "string",
+		"signs_of_mental_illness": "bool",
+		"threat_level":            "string",
+		"flee":                    "string",
+		"body_camera":             "bool",
+	}
+	err = d.LoadAndParse(records, types)
+	if err != nil {
+		panic(err)
+	}
+
+	// parse the date column into Time objects
+	dateStrs := d.GetColumn("date").AsString()
+	dates := make([]time.Time, len(dateStrs))
+	for i := range dateStrs {
+		bits := strings.Split(dateStrs[i], "-")
+		year, _ := strconv.Atoi(bits[0])
+		month, _ := strconv.Atoi(bits[1])
+		day, _ := strconv.Atoi(bits[2])
+		dates[i] = time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
+	}
+
+	// count the number of occurances per week
+	span := dates[len(dates)-1].Sub(dates[0])
+	hoursInWeek := 24 * 7.
+	weeks := int(span.Hours() / hoursInWeek)
+	bins := make(plotter.Values, weeks+1)
+	pts := make(plotter.XYs, weeks+1)
+	w := dates[0]
+	b := 0
+	for i := 0; i < len(dates); {
+		if dates[i].Sub(w).Hours()/24 < 7 {
+			bins[b]++
+			i++
+			pts[b].Y++
+		} else {
+			w = w.AddDate(0, 0, 7)
+			b++
+			pts[b].X = float64(b)
+		}
+	}
+
+	p, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+	p.Title.Text = "Number of Shootings Per Week"
+
+	// try plotting as a histogram
+	h, err := plotter.NewHist(bins, len(bins))
+	if err != nil {
+		panic(err)
+	}
+	p.Add(h)
+
+	if err = p.Save(4*vg.Inch, 4*vg.Inch, "reports/figures/shootings_per_week_hist.png"); err != nil {
+		panic(err)
+	}
+
+	p, err = plot.New()
+	p.Title.Text = "Number of Shootings Per Week"
+	p.X.Label.Text = fmt.Sprintf("Weeks from %s", dates[0])
+	p.Y.Label.Text = "Number of Shootings"
+
+	// try plotting as a line graph
+	err = plotutil.AddLinePoints(p, "Line", pts)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = p.Save(4*vg.Inch, 4*vg.Inch, "reports/figures/shootings_per_week_line.png"); err != nil {
+		panic(err)
+	}
+
+}
