532b5b86b93e1170e68cd183a7371838daf1b975
Matt Poegel <mpoegel@comcast.net>
2016-09-21 20:13:58 -0400

    waffle chart of unarmed by race; started infographic layout

--- src/R/analysis.R
+++ src/R/analysis.R
@@ -1,5 +1,6 @@
 library(extrafont)
 library(ggplot2)
+library(grid)
 library(waffle)
 
 # Read in the first data set
@@ -59,9 +60,8 @@ waffle_theme <- function() {
     legend.text = element_text(family = "Segoe UI", color = dark.blue, size = 10),
     legend.title = element_text(face = "bold", size = 10),
     plot.background = element_rect(fill = gray, color = gray),
-    plot.title = element_text(color = dark.blue, face = "bold", size = 18, vjust = 1, 
-                              family = "Segoe UI"),
-    # panel.background = element_rect(fill = "white"),
+    plot.title = element_text(color = dark.blue, face = "bold", size = 18, vjust = 1,
+                              family = "Segoe UI", margin=margin(1,0,1,0, unit="cm")),
     axis.title = element_text(color = dark.blue, face = "bold", size = 13, family = "Segoe UI"),
     strip.text = element_text(family = "Segoe UI", color = "white"),
     strip.background = element_rect(fill = blue)
@@ -132,7 +132,6 @@ for (race in races) {
   race.counts[race] = x
 }
 
-png("./reports/figures/incidents_by_race.png")
 p2 <- waffle(race.counts / 10,
              rows=7,
              size=0,
@@ -140,9 +139,77 @@ p2 <- waffle(race.counts / 10,
              xlab="1 square = 10 Americans")
 p2 <- p2 + waffle_theme()
 p2
+
+#
+# Plot unarmed or undetermined victims by race
+#
+
+wp.data.unarmed <- wp.data[ wp.data$armed == "unarmed" | wp.data$armed == "undetermined", ]
+unarmed.race.counts <- c()
+for (race in races) {
+  x <- length(wp.data.unarmed[ wp.data.unarmed$race == race, ]$race)
+  unarmed.race.counts[race] = x
+}
+
+p3 <- waffle(unarmed.race.counts,
+             rows=7,
+             size=0,
+             title="Unarmed Americans Killed by Police",
+             xlab="1 square = 1 American")
+p3 <- p3 + waffle_theme()
+p3
+
+png("./reports/figures/incidents_by_race.png")
+p4 <- iron(p2 + theme(legend.position = "none",
+                      plot.title = element_text(margin=margin(0.5,0,0.2,0, unit="cm"))),
+           p3 + theme(plot.title = element_text(margin=margin(1,0,0.2,0, unit="cm"))))
+p4
 dev.off()
 
 
+#
+# Combine the plots together into an infographic
+#
+vplayout <- function(x, y) viewport(layout.pos.row = x, layout.pos.col = y)
+
+png("./reports/figures/infographic1.png", width = 10, height = 20, res = 300, units = "in")
+grid.newpage()
+pushViewport(viewport(layout = grid.layout(nrow=5, ncol=2)))
+grid.rect(gp = gpar(fill = gray, col = gray))
+
+# top banner
+grid.rect(gp = gpar(fill = dark.blue, color = dark.blue),
+          x = unit(0.5, "npc"), y = unit(0.95, "npc"),
+          width = unit(1, "npc"), height = unit(0.15, "npc"))
+# bottom banner
+grid.rect(gp = gpar(fill = dark.blue, color = dark.blue),
+          x = unit(0.5, "npc"), y = unit(0.0, "npc"),
+          width = unit(1, "npc"), height = unit(0.06, "npc"))
+grid.text("Black and Blue", 
+          vjust = 0, hjust = 0,
+          x = unit(0.01, "npc"), y = unit(0.92, "npc"), 
+          gp = gpar(fontfamily = "Segoe UI", col = "white", cex = 8))
+grid.text("An examination of deadly police shootings in America", 
+          vjust = 0, hjust = 0,
+          x = unit(0.01, "npc"), y = unit(0.88, "npc"), 
+          gp = gpar(fontfamily = "Segoe UI", col = "white", cex = 2))
+grid.text("Analysis by Matt Poegel", 
+          vjust = 0, hjust = 0,
+          x = unit(0.01, "npc"), y = unit(0.86, "npc"), 
+          gp = gpar(fontfamily = "Segoe UI", col = blue, cex = 1))
+grid.text("Source: The Washington Post - https://github.com/washingtonpost/data-police-shootings/", 
+          vjust = 0, hjust = 0, 
+          x = unit(0.01, "npc"), y = unit(0.01, "npc"), 
+          gp = gpar(fontfamily = "Segoe UI", col = "white", cex = 1.2))
 
 
+print(p1 + theme(plot.margin=margin(0, 1, 0, 1, unit="cm")), 
+      vp = vplayout(2:3, 1:2))
+print(p2 + theme(plot.margin=margin(0, 1, 0, 1, unit="cm")),
+      vp = vplayout(4, 1))
+print(p3 + theme(legend.position = "none",
+                 plot.margin=margin(-7, 1, 0, 1, unit="cm")), 
+      vp = vplayout(5, 1))
+
+dev.off()
 
